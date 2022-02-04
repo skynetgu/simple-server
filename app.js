d2cbfb55f316ca94904077b4f7c230ba1defb77e
skynet gu <cookitgu@gmail.com>
2022-02-04 18:12:13 +0800

    change greeting

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, "OK OK OK", {
     "Content-Type": "text/plain; charset=UTF-8",
   });
-  res.end("Hello 你好啊");
+  res.end("Hello 你好欧!");
 });
 
 server.listen(PORT, HOSTNAME, () => {
